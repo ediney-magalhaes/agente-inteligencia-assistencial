8b544bcfd285ae2f963c56fdfc7e957e1fa9a9f4
ediney-magalhaes <edy.estatistica@gmail.com>
2026-04-17 13:44:35 -0400

    feat: cria utils.py com constantes, mapeamento de colunas e funções auxiliares

---
+++ utils.py
@@ -0,0 +1,72 @@
+#---------------------------------------------------
+# Script que guarda as funções auxiliares do sistema
+# Criado por: Ediney Magalhães
+# Data: 17/04/2026
+#---------------------------------------------------
+
+# Constantes
+COL_DATA = 'Data da notificação'
+COL_CLASSIFICACAO = 'Classificação da Notificação'
+COL_STATUS = 'Status da notificação'
+COL_PL = 'Unidade responsável pela investigação - PL'
+COL_ACR = 'Unidade responsável pela investigação - ACR'
+COL_PAC = 'Unidade responsável pela investigação - PAC'
+
+# Mapeamento das colunas
+MAPEAMENTO_COLUNAS = {
+    COL_DATA: [],
+    COL_CLASSIFICACAO: [],
+    'Taxonomia da OMS': ['Taxonomia'],
+    'Setor Responsável': ['Responsável'],
+    'Grau do dano': [],
+    'Incidente': [],
+    'Turno': [],
+    'Descrição do incidente': [],
+    'Assistencial/ Administrativo/ Apoio': [],
+    COL_PL: [],
+    COL_ACR: [],
+    COL_PAC: [],
+    COL_STATUS: [],
+    'Escala de Efetividade': []
+}
+
+# Função para atualizar colunas conforme dataframe
+def atualizacao_schema(df):
+    lista = []
+
+    for nome, alternativa in MAPEAMENTO_COLUNAS.items():
+        if nome in df.columns:
+            pass
+        else:
+            for i in alternativa:
+                if i in df.columns:
+                    df.rename(columns= {i:nome},inplace=True)
+                    break
+            else:
+                lista.append(nome)
+    return df, lista
+
+# Função para cálculo da variação de notificações em relação ao trimestre anterior
+def texto_variacao(valor):
+    if valor > 0: return f"um aumento de {valor:.1f}%"
+    elif valor < 0: return f"uma redução de {abs(valor):.1f}%" 
+    else: return "uma estabilidade"
+
+
+# Denominador: Foi encaminhada para tratar?
+def tem_investigacao(row):
+    pl = str(row[COL_PL]).strip().lower()
+    acr = str(row[COL_ACR]).strip().lower()
+    pac = str(row[COL_PAC]).strip().lower()
+    invalidos = ['nan', '-', '', 'nat', 'none']
+    return (pl not in invalidos) or (acr not in invalidos) or (pac not in invalidos)
+
+
+# Numerador: Foi realmente concluída?
+def foi_tratada(row):
+    status = str(row[COL_STATUS]).strip().lower()
+    return ('validado' in status) or ('concluído após a investigação' in status)
+
+# Estatísticas de Ferramentas (PL, ACR, PAC)
+def check_tool(row, col_name):
+    return str(row[col_name]).strip().lower() not in ['nan', '-', '', 'nat', 'none']
