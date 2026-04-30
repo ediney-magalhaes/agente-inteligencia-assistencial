08ba7e69a6150e401e5381edcdf0a7b77a5d5c36
ediney-magalhaes <edy.estatistica@gmail.com>
2026-04-30 09:47:30 -0400

    feat: adiciona analisar_bloco1 ao agente_ia.py

--- agente_ia.py
+++ agente_ia.py
@@ -21,4 +21,30 @@ def chamar_gemini(prompt):
             return resposta.text
         except Exception as e:
             print(f'Modelo {ia} não disponível! Erro {e}')
-    return "Todos os modelos tentados falharam na chamada!"
+    return "Todos os modelos tentados falharam na chamada! Tenta novamente."
+
+# Função para análise do gráfico 1 (Nº notificações)
+def analisar_bloco1(total_atual, total_tri_anterior, var_tri, qtde_mensal, qtde_trimestral):
+    papel = "Especialista em qualidade e segurança do paciente no ambiente hospitalar"
+    contexto = "O hospital Santa Rosa possui o Núcleo de Segurança do Paciente implantado. Uma de suas atribuições é " \
+    "elaborar o Relatório Trimestral de Segurança do Paciente. Esse relatório tem por objetivo trazer análises quantitativas" \
+    "e qualitativas sobre a cultura de segurança do paciente através da ótica do número de notificações realizadas."
+    instrucao = "realizar a comparação entre os meses do ano atual, os meses do ano anterior e o trimestre avaliado. Nesse" \
+    "momento a análise deve focar exclusivamente na variação do número de notificações (aumento ou redução) e possíveis tendências."
+    prompt = f"""
+    Você é {papel}.
+
+    CONTEXTO:
+    {contexto}
+
+    DADOS DO TRIMESTRE:
+    - Total de notificações do trimestre atual: {total_atual}
+    - Total de notificações do trimestre anterior: {total_tri_anterior}
+    - Variação trimestral: {var_tri}
+    - Quantidade mensal: {qtde_mensal}
+    - Quantidade trimestral: {qtde_trimestral}
+
+    INSTRUÇÃO:
+    {instrucao}
+    """
+    return chamar_gemini(prompt)
