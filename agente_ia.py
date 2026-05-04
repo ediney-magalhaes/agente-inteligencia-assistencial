300fb4656580b87294b618affe3e0d1a16985605
ediney-magalhaes <edy.estatistica@gmail.com>
2026-05-04 09:35:47 -0400

    feat: migra agente_ia.py para biblioteca google-genai

--- agente_ia.py
+++ agente_ia.py
@@ -1,5 +1,5 @@
 import os
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 
 # Carrega variáveis de ambiente
@@ -9,15 +9,18 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 # Configuração do cliente google
-genai.configure(api_key=GEMINI_API_KEY)
+cliente = genai.Client(api_key=GEMINI_API_KEY)
 
 # Função padrão de chamada da API
 def chamar_gemini(prompt):
     lista_modelos = ['gemini-2.5-flash', 'gemini-2.5-flash-lite', 'gemini-2.5-pro']
     for ia in lista_modelos:
         try:
-            modelo = genai.GenerativeModel(ia)
-            resposta = modelo.generate_content(prompt)
+            resposta = cliente.models.generate_content(
+                model = ia,
+                contents=prompt
+            )
+            
             return resposta.text
         except Exception as e:
             print(f'Modelo {ia} não disponível! Erro {e}')
